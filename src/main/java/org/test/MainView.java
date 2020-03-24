0352df8e3b84ba783d29557b287ef2239233a8e8
Sean Hellum <seanhellum45@gmail.com>
2020-03-24 23:50:09 +0000

    Add Speed Calculator

--- src/main/java/org/test/MainView.java
+++ src/main/java/org/test/MainView.java
@@ -1,8 +1,17 @@
 package org.test;
 
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.Notification.Position;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Viewport;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.material.Material;
@@ -10,14 +19,63 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @Route
+@Viewport("width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes, viewport-fit=cover")
 @PageTitle("JavaCalc")
 @Theme(value = Material.class, variant = Material.DARK)
 @PWA(name = "JavaCalc", shortName = "JCalc")
-public class MainView extends VerticalLayout {
+public class MainView extends AppLayout {
+
+    private Tabs appTabs = new Tabs(new Tab("Quadratic"), new Tab("Speed"));
 
     public MainView() {
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
-        add(button);
+        addToNavbar(true, new DrawerToggle());
+        this.appTabs.setOrientation(Tabs.Orientation.HORIZONTAL);
+        addToDrawer(this.appTabs);
+        setContent(selectLayout());
+        this.appTabs.addSelectedChangeListener(event -> {
+            setContent(selectLayout());
+        });
+    }
+
+    public VerticalLayout selectLayout() {
+        switch (this.appTabs.getSelectedIndex()) {
+        case 0:
+            return createQuadraticLayout();
+        case 1:
+            return createSpeedLayout();
+        }
+        return null;
+    }
+
+    public VerticalLayout createSpeedLayout() {
+        VerticalLayout layout = new VerticalLayout();
+        TextField timeField = new TextField("Time");
+        TextField distancField = new TextField("Distance");
+        Button submitButton = new Button("Submit", new Icon(VaadinIcon.ARROW_RIGHT), event -> {
+            int distance = Integer.parseInt(distancField.getValue());
+            int time = Integer.parseInt(timeField.getValue());
+            int speed = distance / time;
+            Notification.show("Speed: " + Integer.toString(speed), 3000, Position.BOTTOM_END);
+            timeField.clear();
+            distancField.clear();
+        });
+        submitButton.setIconAfterText(true);
+        layout.add(distancField, timeField, submitButton);
+        return layout;
+    }
+
+    public VerticalLayout createQuadraticLayout() {
+        VerticalLayout layout = new VerticalLayout();
+        TextField aField = new TextField("A");
+        TextField bField = new TextField("B");
+        TextField cField = new TextField("C");
+        Button submitButton = new Button("Submit", new Icon(VaadinIcon.ARROW_RIGHT), event -> {
+            aField.clear();
+            bField.clear();
+            cField.clear();
+        });
+        submitButton.setIconAfterText(true);
+        layout.add(aField, bField, cField, submitButton);
+        return layout;
     }
 }
